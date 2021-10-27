9af686387f356e8bbda0f0039a4df7de5037c517
rogrp <rogrp6@gmail.com>
2021-10-27 01:35:45 -0500

    add comments, TODO: fix race condition

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,22 @@ from django.template import loader
 from django.urls import reverse
 from polls.models import Choice, Question
 
+"""
+TODO: Fix the RACE CONDITION:
+
+The code for our vote() view does have a small problem. It first gets 
+the selected_choice object from the database, then computes the new 
+value of votes, and then saves it back to the database. If two users
+of your website try to vote at exactly the same time, this might go
+wrong: The same value, let’s say 42, will be retrieved for votes. Then, 
+for both users the new value of 43 is computed and saved, but 44 
+would be the expected value.
+
+This is called a race condition. If you are interested, you can read 
+Avoiding race conditions using F() to learn how you can solve this issue.
+
+"""
+
 def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
   return render(request, 'polls\index.html', {'latest_question_list': latest_question_list})
