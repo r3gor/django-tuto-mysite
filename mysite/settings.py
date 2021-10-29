0d4780b40b646ae71786b3a274c2cd0b8e83e499
rogrp <rogrp6@gmail.com>
2021-10-28 19:30:40 -0500

    allowe host set *

--- mysite/settings.py
+++ mysite/settings.py
@@ -142,4 +142,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-ALLOWED_HOSTS = ['django-tuto.herokuapp.com']
+ALLOWED_HOSTS = ['django-tuto.herokuapp.com', '*']
