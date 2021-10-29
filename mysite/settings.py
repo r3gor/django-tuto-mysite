eec25148881a37365e6d6d21be8f93edb81f380a
rogrp <rogrp6@gmail.com>
2021-10-28 19:22:42 -0500

    heroku to allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -141,3 +141,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+ALLOWED_HOSTS = ['django-tuto.herokuapp.com']
